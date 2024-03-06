08983f1154d5ea42d8ef8b3e241c5bdfc80c87d9
OuattaraFatogoma <ouattarafatogmaabdoulaziz@gmail.com>
2024-03-06 17:04:05 +0100

    enable websocket client side

--- frontend/src/context.jsx
+++ frontend/src/context.jsx
@@ -1,10 +1,34 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
 
 const ContextApi = createContext();
 
 export const ContextProvider = ({children}) => {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
     const [selectConversation, setSelectConversation] = useState(0);
+    const [socket, setSocket] = useState(null);
+
+
+    useEffect(()=>{
+        if(user){
+            console.log(user);
+            const socket = io("http://localhost:5000",{
+                query: {
+                  user_id: user.id,
+                }
+            });
+            
+            setSocket(socket);
+
+        }else{
+            if(socket){
+                socket.close();
+                setSocket(null);
+            }
+        }
+    }, [user])
+
 
     return(
         <ContextApi.Provider value={{user, setUser, selectConversation, setSelectConversation}}>
